bf375ef4671045a280fbbef149fa69d5e7582584
Sasiru Liyanage <it24200741@my.sliit.lk>
2025-09-16 23:10:20 +0530

    IT24200741(Lab 07)

---
+++ Lab Sheet 07/IT24200741.R
@@ -0,0 +1,21 @@
+setwd("D:\\SLIIT\\Y2 S1\\PS\\IT24200741")
+
+# EX 02
+
+## Question 01
+punif(10, min = 0, max = 40, lower.tail = FALSE) - punif(25, min = 0, max = 40, lower.tail = FALSE)
+
+## Question 02
+
+pexp(2, rate = 1/3,lower.tail = TRUE)
+
+## Question 03
+
+### Part 01
+
+1-pnorm(130,mean = 100, sd = 15, lower.tail = TRUE)
+
+### Part 02
+
+qnorm(0.95, mean=100, sd=15)
+
