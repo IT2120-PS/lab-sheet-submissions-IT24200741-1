8e96dd1961220aaf13557c9dfa541e82f411c950
Sasiru Liyanage <it24200741@my.sliit.lk>
2025-09-16 23:23:28 +0530

    IT24200741(Lab 06)

---
+++ Lab Sheet 06/IT24200741_Lab_06.R
@@ -0,0 +1,22 @@
+# Exercise 
+setwd("D:\\SLIIT\\Y2 S1\\PS\\IT24200741")
+
+###Question 01
+
+## i. Distribution of X
+# Binomial Distribution
+
+## ii. Probability that ≥ 47 students passed
+pbinom(35, 44, 0.92, lower.tail = TRUE)
+
+###Question 02
+
+## i. Random variable X
+# Number of calls received per hour
+
+## ii. Distribution of X
+# Poisson Distribution
+
+## iii. Probability that exactly 15 calls are received
+dpois(15, 12)
+
